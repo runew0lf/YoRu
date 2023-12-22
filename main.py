5556d1e6606ebeedeb3b4c5025d0ebb25d5648fa
Runew0lf <runew0lf@gmail.com>
2023-12-22 16:08:49 +0000

    updated requirements.txt

--- main.py
+++ main.py
@@ -12,24 +12,28 @@ thisfile = st.image(
     "YoRu.png",
     width=600,
 )
-prompttext = st.text_area(
-    "prompt", value="masterpiece, best quality, man", label_visibility="visible"
-)
 
-if st.button(label="Generate"):
-    client.connect()
-    with open("workflows\standard_sdxl.json", "r") as file:
-        prompt = json.load(file)
+col1, col2 = st.columns([3, 1])
 
-    prompt["6"]["inputs"]["text"] = prompttext
-    prompt["3"]["inputs"]["seed"] = random.randint(0, 1000000)  # random
-    prompt["5"]["inputs"]["width"] = 1024
-    prompt["5"]["inputs"]["height"] = 1024
-    images_dict = client.get_images(prompt)
-    for node_id, images in images_dict.items():
-        for image_data in images:
-            thisfile.image(image_data, width=600)
-    # for image_data in client.get_images(prompt):
-    #     image = Image.open(io.BytesIO(image_data))
-    #     thisfile.image(image, width=600)
-    client.disconnect()
+with col1:
+    prompttext = st.text_area(
+        "prompt", value="masterpiece, best quality, man", label_visibility="visible"
+    )
+with col2:
+    if st.button(label="Generate"):
+        client.connect()
+        with open("workflows\standard_sdxl.json", "r") as file:
+            prompt = json.load(file)
+
+        prompt["6"]["inputs"]["text"] = prompttext
+        prompt["3"]["inputs"]["seed"] = random.randint(0, 1000000)  # random
+        prompt["5"]["inputs"]["width"] = 1024
+        prompt["5"]["inputs"]["height"] = 1024
+        images_dict = client.get_images(prompt)
+        for node_id, images in images_dict.items():
+            for image_data in images:
+                thisfile.image(image_data, width=600)
+        # for image_data in client.get_images(prompt):
+        #     image = Image.open(io.BytesIO(image_data))
+        #     thisfile.image(image, width=600)
+        client.disconnect()

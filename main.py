2dce07919f7522319790c5ae342db340815554f2
Runew0lf <runew0lf@gmail.com>
2023-12-23 09:28:32 +0000

    layout testing

--- main.py
+++ main.py
@@ -5,52 +5,64 @@ from PIL import Image
 import io
 import random
 
+# https://docs.streamlit.io/
+
+
 client = WebSocketClient()
 
-steps = st.sidebar.slider("Steps", 1, 100, 20)
-cfg = st.sidebar.slider("Cfg", 1, 20, 7)
+st.set_page_config(layout="wide", page_title="YoRu", page_icon="🤖")
 
-thisfile = st.image("YoRu.png", use_column_width="always")
-my_bar = st.progress(0)
-col1, col2 = st.columns([3, 1])
+_, main_column, right_column = st.columns([2, 6, 2])
 
-with col1:
-    prompttext = st.text_area(
-        "prompt", value="masterpiece, best quality, man", label_visibility="visible"
-    )
-with col2:
-    if st.button(label="Generate"):
-        with WebSocketClient() as client:
-            with open("workflows\standard_sdxl.json", "r") as file:
-                prompt = json.load(file)
 
-            ksampler = prompt["3"]["inputs"]
+steps = right_column.slider("Steps", 1, 100, 20)
+cfg = right_column.slider("Cfg", 1, 20, 7)
 
-            prompt["6"]["inputs"]["text"] = prompttext
-            ksampler["seed"] = random.randint(0, 1000000)  # random
-            ksampler["steps"] = steps
-            ksampler["cfg"] = cfg
-            prompt["5"]["inputs"]["width"] = 1024
-            prompt["5"]["inputs"]["height"] = 1024
+with main_column:
+    _, indent, _ = st.columns([1, 3, 1])
+    thisfile = indent.image("YoRu.png", use_column_width=True)
+    my_bar = indent.progress(0)
+    prompt_column, button_column = st.columns([6, 1])
 
-            my_bar.progress(0, f"Loading Model...")
-            for item in client.get_images(prompt):
-                if client.status != "done":
-                    progress = int((100 / ksampler["steps"]) * item)
-                    my_bar.progress(
-                        progress, f"Generating... {item} / {ksampler['steps']}"
-                    )
-                    print(client.status.upper())
-                    if client.status == "executing":  ##Doesnt quite work yet
+    with prompt_column:
+        prompttext = st.text_area(
+            "prompt", value="masterpiece, best quality, man", label_visibility="hidden"
+        )
+    with button_column:
+        st.write("#")
+        if st.button(label="Generate", use_container_width=True):
+            with WebSocketClient() as client:
+                with open("workflows\standard_sdxl.json", "r") as file:
+                    prompt = json.load(file)
+
+                ksampler = prompt["3"]["inputs"]
+
+                prompt["6"]["inputs"]["text"] = prompttext
+                ksampler["seed"] = random.randint(0, 1000000)  # random
+                ksampler["steps"] = steps
+                ksampler["cfg"] = cfg
+                prompt["5"]["inputs"]["width"] = 1024
+                prompt["5"]["inputs"]["height"] = 1024
+
+                my_bar.progress(0, f"Loading Model...")
+                for item in client.get_images(prompt):
+                    if client.status != "done":
+                        progress = int((100 / ksampler["steps"]) * item)
                         my_bar.progress(
-                            progress, f"Current Node: {client.current_node}"
+                            progress, f"Generating... {item} / {ksampler['steps']}"
                         )
+                        print(client.status.upper())
+                        if client.status == "executing":  ##Doesnt quite work yet
+                            my_bar.progress(
+                                progress, f"Current Node: {client.current_node}"
+                            )
 
-                    if client.preview is not None:
-                        image = Image.open(io.BytesIO(client.preview[8:]))
-                        thisfile.image(image, use_column_width="always")
+                        if client.preview is not None:
+                            image = Image.open(io.BytesIO(client.preview[8:]))
+                            thisfile.image(image, use_column_width="always")
 
-            for node_id, images in item.items():
-                for image_data in images:
-                    thisfile.image(image_data, use_column_width="always")
-        my_bar.progress(0)
+                for node_id, images in item.items():
+                    for image_data in images:
+                        thisfile.image(image_data, use_column_width="always")
+            my_bar.progress(0)
+        st.button(label="Stop", use_container_width=True)

a918d1c7d30481525fa8993f1120ab20fde6c1e6
Runew0lf <runew0lf@gmail.com>
2024-01-24 19:00:42 +0000

    put in asyncio.sleep so it doesnt lose connection

--- ng_main.py
+++ ng_main.py
@@ -7,6 +7,7 @@ from nicegui import run, ui
 from nicegui.background_tasks import create
 
 from fastapi import BackgroundTasks, FastAPI
+import asyncio
 
 from modules.settings import resolutions, styles
 from modules.prompt_processing import process_prompt
@@ -64,11 +65,11 @@ def generate_clicked():
 
 async def call_comfy():
     workflow = load_workflow("standard_sdxl.json")
-# FIXME use lora(s)
-#    if lora_model != "None":
-#        workflow = load_workflow("sdxl_lora.json")
-#        workflow["10"]["inputs"]["lora_name"] = lora_model
-#        workflow["10"]["inputs"]["strength_model"] = lora_strength
+    # FIXME use lora(s)
+    #    if lora_model != "None":
+    #        workflow = load_workflow("sdxl_lora.json")
+    #        workflow["10"]["inputs"]["lora_name"] = lora_model
+    #        workflow["10"]["inputs"]["strength_model"] = lora_strength
 
     ksampler = workflow["3"]["inputs"]
 
@@ -83,39 +84,42 @@ async def call_comfy():
     ksampler["sampler_name"] = gen_data["sampler"]
     ksampler["scheduler"] = gen_data["scheduler"]
 
-# FIXME scale this according to the type of model?
-#    workflow["5"]["inputs"]["width"] = resolutions[resolution_picker]["width"]
-#    workflow["5"]["inputs"]["height"] = resolutions[resolution_picker]["height"]
+    # FIXME scale this according to the type of model?
+    #    workflow["5"]["inputs"]["width"] = resolutions[resolution_picker]["width"]
+    #    workflow["5"]["inputs"]["height"] = resolutions[resolution_picker]["height"]
     workflow["5"]["inputs"]["width"] = 1024
     workflow["5"]["inputs"]["height"] = 1024
 
     workflow["5"]["inputs"]["batch_size"] = gen_data["no_of_images"]
-    workflow["4"]["inputs"]["ckpt_name"] = Path(gen_data["model"].replace(".jpeg", ".safetensors")).name
+    workflow["4"]["inputs"]["ckpt_name"] = Path(
+        gen_data["model"].replace(".jpeg", ".safetensors")
+    ).name
 
-#    with st.spinner():
+    #    with st.spinner():
     if True:
-#FIXME
-#        my_bar.progress(0, f"Loading Model...")
+        # FIXME
+        #        my_bar.progress(0, f"Loading Model...")
         for item in client.get_images(workflow):
+            await asyncio.sleep(0.1)
             if client.status != "done":
                 progress = int((100 / ksampler["steps"]) * item)
-#FIXME
-#                my_bar.progress(
-#                    progress, f"Generating... {item} / {ksampler['steps']}"
-#                )
+                # FIXME
+                #                my_bar.progress(
+                #                    progress, f"Generating... {item} / {ksampler['steps']}"
+                #                )
                 print(client.status.upper())
-# FIXME
+                # FIXME
                 if client.status == "executing":  ##Doesnt quite work yet
                     print(f"Thinking...")
-#                    my_bar.progress(
-#                        progress, f"Current Node: {client.current_node}"
-#                    )
+                #                    my_bar.progress(
+                #                        progress, f"Current Node: {client.current_node}"
+                #                    )
 
                 if client.preview is not None:
-#                    st.session_state.image = convert_bytes_to_PIL(client.preview)
-#                    mainimage.image(
-#                        st.session_state.image, use_column_width="always"
-#                    )
+                    #                    st.session_state.image = convert_bytes_to_PIL(client.preview)
+                    #                    mainimage.image(
+                    #                        st.session_state.image, use_column_width="always"
+                    #                    )
                     tmp_image = convert_bytes_to_PIL(client.preview)
                     mainimage.set_source(tmp_image)
 
@@ -123,13 +127,15 @@ async def call_comfy():
             for image_data in images:
                 tmp_image = convert_bytes_to_PIL(client.preview)
                 mainimage.set_source(tmp_image)
-                #st.session_state.image = image_data
-                #mainimage.image(st.session_state.image, use_column_width="always")
+                # st.session_state.image = image_data
+                # mainimage.image(st.session_state.image, use_column_width="always")
 
 
 def stop_clicked():
     ui.notify(f"DEBUG: Stop!")
-    requests.post(f"http://127.0.0.1:8188/interrupt", data="x") # FIXME connect to remote comfyui
+    requests.post(
+        f"http://127.0.0.1:8188/interrupt", data="x"
+    )  # FIXME connect to remote comfyui
 
 
 # User interface
@@ -242,9 +248,13 @@ with ui.row().classes("w-full no-wrap"):
                                     ui.image(
                                         str(modelimage),
                                     ).style("fit: cover; aspect-ratio: 1")
-                                    ui.label(modelname.replace(".safetensors", "")).classes(
+                                    ui.label(
+                                        modelname.replace(".safetensors", "")
+                                    ).classes(
                                         "absolute-bottom text-subtitle2 text-center"
-                                    ).style("white-space: nowrap; overflow: hidden; text-overflow: ellipsis")
+                                    ).style(
+                                        "white-space: nowrap; overflow: hidden; text-overflow: ellipsis"
+                                    )
 
     # with tabs[tab_names.index("Model")]:
     #    models = client.object_info("CheckpointLoaderSimple", "ckpt_name")[0]
@@ -300,7 +310,9 @@ with ui.row().classes("w-full no-wrap"):
         with ui.row().classes("w-full"):
             status = ui.label("").style("position: absolute")
         with ui.row().classes("w-full"):
-            mainimage = ui.image("resources/YoRu.png").props("fit=scale-down height=90vh")
+            mainimage = ui.image("resources/YoRu.png").props(
+                "fit=scale-down height=90vh"
+            )
 
 #    _, indent, _ = st.columns([1, 3, 1])
 #    if "image" not in st.session_state:
